360ecc855b014ea1cc3389e2d3985e7d00f0ddd3
Marju Hirsh <marjutubli@gmail.com>
2020-05-29 13:22:11 +0100

    broken shouty

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'shouty'
-require 'coordinate'
+# require 'shouty'
+# require 'coordinate'
 
-ARBITRARY_MESSAGE = 'Hello, world'
+# ARBITRARY_MESSAGE = 'Hello, world'
 
-Before do
-  @shouty = Shouty.new
-end
+# Before do
+#   @shouty = Shouty.new
+# end
 
-Given('Lucy is at {int}, {int}') do |x, y|
-  @shouty.set_location('Lucy', Coordinate.new(x, y))
-end
+# Given('Lucy is at {int}, {int}') do |x, y|
+#   @shouty.set_location('Lucy', Coordinate.new(x, y))
+# end
 
-Given('Sean is at {int}, {int}') do |x, y|
-  @shouty.set_location('Sean', Coordinate.new(x, y))
-end
+# Given('Sean is at {int}, {int}') do |x, y|
+#   @shouty.set_location('Sean', Coordinate.new(x, y))
+# end
 
-When('Sean shouts') do
-  @shouty.shout('Sean', ARBITRARY_MESSAGE)
-end
+# When('Sean shouts') do
+#   @shouty.shout('Sean', ARBITRARY_MESSAGE)
+# end
 
-Then('Lucy should hear Sean') do
-  expect(@shouty.shouts_heard_by('Lucy').size).to eq(1)
-end
+# Then('Lucy should hear Sean') do
+#   expect(@shouty.shouts_heard_by('Lucy').size).to eq(1)
+# end
 
-Then('Lucy should hear nothing') do
-  expect(@shouty.shouts_heard_by('Lucy').size).to eq(0)
-end
+# Then('Lucy should hear nothing') do
+#   expect(@shouty.shouts_heard_by('Lucy').size).to eq(0)
+# end
