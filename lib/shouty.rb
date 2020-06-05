2a9ed00587d31b056fc10548685ad7902700ffe3
Marju Hirsh <marjutubli@gmail.com>
2020-06-05 12:50:32 +0100

    no features this time

--- lib/shouty.rb
+++ lib/shouty.rb
@@ -1,35 +1,16 @@
+# frozen_string_literal: true
+
 require_relative './coordinate'
 
 class Shouty
-    MESSAGE_RANGE = 1000
+  def shouts_heard_by(listener)
+    shoutsHeard = {}
 
-    def initialize
-      @locations = {}
-      @shouts = {}
+    @shouts.each do |shouter, shouts|
+      distance = @locations[listener].distance_from(@locations[shouter])
+      shoutsHeard[shouter] = shouts if distance < MESSAGE_RANGE
     end
 
-    def set_location(person, location)
-      @locations[person] = location
-    end
-
-    def shout(person, shout)
-      if (!@shouts.key?(person))
-        @shouts[person] = []
-      end
-
-      @shouts[person].push(shout)
-    end
-
-    def shouts_heard_by(listener)
-      shoutsHeard = {}
-
-      @shouts.each do |shouter, shouts|
-        distance = @locations[listener].distance_from(@locations[shouter])
-        if (distance < MESSAGE_RANGE)
-          shoutsHeard[shouter] = shouts
-        end
-      end
-
-      shoutsHeard
-    end
+    shoutsHeard
+  end
 end
